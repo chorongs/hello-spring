0081fbf42c8c764313bccdf0ea099af9cb951fd6
chorongs <charmblues@gmail.com>
2023-04-12 23:52:39 +0900

    자바로 빈 등록

---
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -0,0 +1,20 @@
+package hello.hellospring.service;
+
+import hello.hellospring.repositroy.MemberRepository;
+import hello.hellospring.repositroy.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
